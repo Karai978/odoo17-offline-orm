ed397d6e82a39745ddf3b385d84ff40c98e68af6
valimbavaka-hub <valimbavakarajonasy@gmail.com>
2026-09-15 10:52:09 +0300

    Add Odoo 17 addon manifest and backend assets

---
+++ offline_orm/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Odoo 17 Offline ORM",
+    "version": "17.0.1.0.0",
+    "summary": "Native Odoo 17 ORM routing between RPC and local IndexedDB",
+    "category": "Technical",
+    "license": "LGPL-3",
+    "depends": ["web"],
+    "assets": {
+        "web.assets_backend": [
+            "offline_orm/static/src/offline_orm_service.js",
+            "offline_orm/static/src/connection_router.js",
+            "offline_orm/static/src/offline_database.js",
+            "offline_orm/static/src/offline_query_engine.js",
+            "offline_orm/static/src/register_service.js",
+        ],
+    },
+    "installable": True,
+    "application": False,
+}
