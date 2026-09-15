5a84b513b4444e4e76662d9121f7c6394dfae5ed
valimbavaka-hub <valimbavakarajonasy@gmail.com>
2026-09-15 10:57:26 +0300

    Register offline bootstrap service

---
+++ offline_orm/static/src/register_bootstrap.js
@@ -0,0 +1,4 @@
+import { registry } from "@web/core/registry";
+import { offlineBootstrapService } from "./offline_bootstrap";
+
+registry.category("services").add("offline_bootstrap", offlineBootstrapService);
